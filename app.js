83d5a1e63f41a61e423c9a814e0e107affbdc233
TimNurMur <tim.nur-mur@mail.ru>
2023-04-21 11:09:16 +0400

    refactor: replaced the class name to rule 3.1

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ var createNewTaskElement=function(taskString){
     //Each elements, needs appending
     checkBox.type="checkbox";
     editInput.type="text";
-    editInput.className="task";
+    editInput.className="task task-input-text";
 
     editButton.innerText="Edit"; //innerText encodes special characters, HTML does not.
     editButton.className="edit-button custom-button";
@@ -82,7 +82,7 @@ var editTask=function(){
 
     var listItem=this.parentNode;
 
-    var editInput=listItem.querySelector('input[type=text]');
+    var editInput=listItem.querySelector(".task-input-text");
     var label=listItem.querySelector("label");
     var editBtn=listItem.querySelector(".edit-button");
     var containsClass=listItem.classList.contains("edit-mode");
